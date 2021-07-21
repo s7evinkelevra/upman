d3c158e28a0281b4c2c103004cd313d2cf1d924c
s7evinkelevra <kelevra.1337@gmail.com>
2021-07-21 12:21:39 +0200

    added node-cron instead of cron to not run node as root

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
+const cron = require('node-cron');
 
 
 const sites = [
-/*   "http://brkhnudd.myraidbox.de/",
+  "http://brkhnudd.myraidbox.de/",
   "http://b2rzk986.myraidbox.de/",
   "http://b3mfy0.myraidbox.de/",
-  "http://b3mfy0.myraidbox.de/", */
+  "http://b3mfy0.myraidbox.de/",
   "https://ganteroptik.de/",
   "https://augenoptik-sabrina-buhl.de/",
   "https://luedemann2.de/",
@@ -42,32 +43,38 @@ let transporter = nodemailer.createTransport({
   newline: 'unix'
 });
 
-// quick hack, make sure added items are unique
-const fdSites = new Set();
-
-(async () => {
-  const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser'
-  });
-
-  const page = await browser.newPage();
-
-  await page.on('requestfailed', (request) => {
-    // do nothing if the filename contains mp4 since mp4 encoders are not shipped by puppeteer
-    const filetype = request.url().split(".").pop();
-    if(ignoredFileTypes.includes(filetype)) return;
-
-    console.log(`${request.failure().errorText} ${request.url()}`)
-    fdSites.add(request.headers().referer);
-  });
-
-  for (const site of sites) {
-    await page.goto(site, {
-      waitUntil: 'networkidle2'
+const main = async () => {
+  // quick hack, make sure added items are unique
+  const fdSites = new Set();
+  try{
+    const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/chromium-browser'
     });
+
+    const page = await browser.newPage();
+
+    await page.on('requestfailed', (request) => {
+      // do nothing if the filename contains mp4 since mp4 encoders are not shipped by puppeteer
+      const filetype = request.url().split(".").pop();
+      if (ignoredFileTypes.includes(filetype)) return;
+
+      console.log(`${request.failure().errorText} ${request.url()}`)
+      fdSites.add(request.headers().referer);
+    });
+
+    for (const site of sites) {
+      await page.goto(site, {
+        waitUntil: 'networkidle2'
+      });
+    }
+
+    await browser.close();
+
+  }catch(err){
+    console.log("ERRRRR");
+    console.log(err);
   }
-  
-  await browser.close();
+
 
   if(fdSites.size > 0) {
     // spread items to array since sets cant be joined like that
@@ -83,4 +90,9 @@ const fdSites = new Set();
       console.log(info.messageId);
     });
   }
-})();
+};
+
+
+cron.schedule('0 */6 * * *', async () => {
+  main();
+})
